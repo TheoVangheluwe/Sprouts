5dc8b74f9bd08523e25e4cbce4f8492279990944
theo-stordeur <theo.stordeur@student.junia.com>
2025-03-31 14:57:08 +0200

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-uranianBlue text-gray-900 text-center p-4 shadow-md">
-      © 2024 Sprouts Game. Tous droits réservés.
+      © 2025 Sprouts Game. Tous droits réservés.
     </footer>
   );
 };
