984320a250e9dcfbc8a35bbd5a5d0236b18cb5cb
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2025-04-07 15:27:04 +0200

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './sprouts.png';import { Link } from 'react-router-dom';
+import logo from './sprouts.png';
+//import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
