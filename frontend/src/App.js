046b1cae0fb667735b5af0b360536d5c2a2a33b9
Quentin PRUVOT <quentin.pruvot@student.junia.com>
2025-04-11 17:09:17 +0200

    min fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import GamePage from './pages/GamePage';
 import HomePage from './pages/HomePage';
 import RulesPage from './pages/RulesPage';
 import WaitingRoom from './components/game/WaitingRoom';
-import OnlineGame from './components/Online/OnlineGame';
+import OnlineGame from './components/online/OnlineGame';
 import './App.css';
 
 const App = () => {
@@ -25,7 +25,6 @@ const App = () => {
         </main>
         <Footer />
       </div>
-        
     </Router>
   );
 };
