be81f86e4110d1797732465cb7d4974ebb00f4d8
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2025-04-18 13:11:27 +0200

    refactor: ajout d'un style jeux

--- frontend/src/pages/MenuPage.js
+++ frontend/src/pages/MenuPage.js
@@ -1,33 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MenuPage = () => {
   return (
-    <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-md w-full">
-        <h1 className="text-4xl font-bold mb-6">Menu Principal</h1>
-        <div className="space-y-4">
+    <div className="bg-gradient-to-br from-gray-900 to-black flex flex-col items-center justify-center min-h-screen p-4 font-arcade">
+      <div className="bg-gray-800 border-4 border-yellow-400 p-8 rounded-lg shadow-2xl text-center max-w-md w-full">
+        <h1 className="text-4xl font-bold mb-8 text-yellow-300 animate-pulse">🎮 Menu 🎮</h1>
+        <div className="space-y-6">
           <Link
             to="/pve"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 block"
+            className="bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-500 transform transition hover:scale-105 shadow-md w-full block"
           >
-            Joueur contre Joueur (Local)
+            Joueur contre Joueur (LAN)
           </Link>
           <Link
             to="/ai"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 block"
+            className="bg-green-600 text-white py-3 px-6 rounded-lg hover:bg-green-500 transform transition hover:scale-105 shadow-md w-full block"
           >
             Joueur contre IA (Local)
           </Link>
           <Link
             to="/waiting-room"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 block"
+            className="bg-purple-600 text-white py-3 px-6 rounded-lg hover:bg-purple-500 transform transition hover:scale-105 shadow-md w-full block"
           >
             Jeux Multi
           </Link>
           <Link
             to="/settings"
-            className="bg-gray-300 text-gray-800 py-2 px-4 rounded hover:bg-gray-400 block"
+            className="bg-gray-600 text-white py-3 px-6 rounded-lg hover:bg-gray-500 transform transition hover:scale-105 shadow-md w-full block"
           >
             Paramètres
           </Link>
